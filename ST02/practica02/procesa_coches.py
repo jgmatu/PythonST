93464128ba21f4801486a51aff52bbbe320f8e5c
jgmatu <fj.gutierrezs91@gmail.com>
2016-03-07 10:51:23 +0100

    last changes

--- ST02/practica02/procesa_coches.py
+++ ST02/practica02/procesa_coches.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python -tt
 # -*- coding: utf-8 -*-
 
+#python UnicodeEncodeError: 'ascii' codec can't encode character in position : ordinal not in range(128)
 
 import sys , os , shutil
 import argparse
 import xml.etree.ElementTree as ET
 
+def printAttr (attr) :
+    for x in attr.keys() :
+        print u"Attribute   : ",
+        print x  ,
+        print u"  Value : "  ,
+        print attr[x]
+        #indice = 0
+        #while indice < len(attr[x]) :
+        #    letra = attr[x][indice]
+        #    print unicode(letra)
+        #    indice = indice + 1
 
 def formatfile (tag  , attr , text) :
-        print "Etiqueta : " , tag
-        print "Atributos : " ,attr
-        print "Texto : " , text
+        print u"Etiqueta : " , tag
+        #printAttr(attr)
+        print attr
+        if text != None :
+            print u"Texto : " , text
 
 
 def getXML(filename) :
@@ -20,18 +34,18 @@ def getXML(filename) :
 
 
 def main() :
-    usage = "%prog [OPCIONES] input output if not arguments we read stdin"
+    usage = u"%prog [OPCIONES] input output if not arguments we read stdin"
     parser = argparse.ArgumentParser(usage)
 
     parser.add_argument("-i" , "--input"  , action="store" ,     dest="input"  , help = "xml file to format")
     arguments = parser.parse_args()
 
     if arguments.input == None :
-        print "read from stdin"
+        print u"read from stdin"
     else :
-        print "read from file " + arguments.input
+        print u"reading from file " + arguments.input
         if not os.path.exists(arguments.input) :
-            print "File : " + arguments.input + " not exist."
+            print u"File : " + arguments.input + u" not exist."
             raise SystemExit
         getXML(arguments.input)
 
