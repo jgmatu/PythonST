ffa07bae6bf28cd49d4af361adc514302a3383d1
jgmatu <fj.gutierrezs91@gmail.com>
2016-03-12 23:38:25 +0100

    changes in st02

--- ST02/practica02/procesa_discoteca.py
+++ ST02/practica02/procesa_discoteca.py
@@ -1,27 +1,52 @@
 #!/usr/bin/python -tt
 # -*- coding: utf-8 -*-
 
+
 import sys , os , shutil
 import argparse
 import xml.etree.ElementTree as ET
 
-MAX = 12
+MAX = 55
+
+
+def printnumSong (numSong) :
+    if int(numSong) < 10 :
+        print numSong + '  ' ,
+    elif int(numSong) < 100 :
+        print numSong + ' ' ,
+    else :
+        print numSong + '',
 
 def printspaces (lenword) :
     for x in range(MAX - lenword) :
         print '',
 
-def formatfile (element , attr , text) :
-    for x in attr.keys() :
+def formatfile (attr , numsong) :
+    printnumSong(str(numsong))
+    printspaces(40)
+    keys = attr.keys()
+    keys.reverse()
+    for x in keys :
         print attr[x].upper() ,
-        printspaces(len(attr[x]))
+        if not x in "duracion" :
+            printspaces(len(attr[x]))
     print
 
+def printborder () :
+    print '-------'*MAX
 
-def prhead () :
-    print "Matricula    " + "Modelo       " + "Marca       "
-    print "---------" + 8*'-' + ------ + 8*'-' + "----------"
+def printheader (attr) :
+    keys = attr.keys()
+    keys.sort()
 
+    printborder()
+    for x in keys :
+        print attr[x].upper() ,
+
+        if not x in "titulo" :
+            print ' '*MAX ,
+    print
+    printborder()
 
 def getXML(filename) :
 
@@ -31,10 +56,17 @@ def getXML(filename) :
         else :
             root = ET.ElementTree(file=sys.stdin).getroot()
 
-        prhead()
+        numsong = 1
         for elemento in root.iter() :
+
+            if elemento.tag in "disco" :
+                printheader(elemento.attrib)
+                numsong = 1
+                continue
+
             if root.tag != elemento.tag :
-                formatfile(elemento.tag , elemento.attrib , elemento.text)
+                formatfile(elemento.attrib , numsong)
+                numsong = numsong + 1
 
     except ET.ParseError :
         sys.stderr.write("Error process file xml not well formed file xml" + '\n')
@@ -50,12 +82,12 @@ def main() :
 
     if arguments.input == None :
         getXML(None)
+
     else :
 
         if not os.path.exists(arguments.input) :
             print u"File : " + arguments.input + u" not exist."
             raise SystemExit
-
         getXML(arguments.input)
 
 
